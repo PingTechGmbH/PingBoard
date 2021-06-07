af34077974f3dff6f92fe0b7089ee21eaefff381
Maximilian Deubel <maximilian.deubel@gmail.com>
2021-06-07 18:35:50 +0200

    Change blink script to not require color for OFF command

--- Prototype/ColorSetter/pingboard-blink.py
+++ Prototype/ColorSetter/pingboard-blink.py
@@ -13,16 +13,20 @@ def find_arduino():
 
 
 def main():
-    if len(sys.argv) != 6:
+    if len(sys.argv) == 6:
+        r =  int(sys.argv[3])
+        g =  int(sys.argv[4])
+        b =  int(sys.argv[5])
+    elif len(sys.argv) == 3 and sys.argv[2] == 'OFF':
+        r =  0
+        g =  0
+        b =  0
+    elif len(sys.argv) != 6:
         print("Must have five arguments: <switch> <mode> <red> <green> <blue>")
         sys.exit(-1)
 
     sw = int(sys.argv[1])
     mode =   sys.argv[2]
-    r =  int(sys.argv[3])
-    g =  int(sys.argv[4])
-    b =  int(sys.argv[5])
-
 
     cmd_string = "BLNK {:1d} {} {:03d} {:03d} {:03d}\n".format(sw, mode, r, g, b)
 
